ab192dddb3e330a84886597a3b17b5c01d475d22
Enichka <84974312+Enichka@users.noreply.github.com>
2024-07-24 21:13:08 +0300

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
-import 'package:flutter/material.dart';
+import 'dart:ffi';
 
-void main() {
-  runApp(const ExampleApp());
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: FirstPage());
+  }
 }
 
-class ExampleApp extends StatefulWidget {
+class FirstPage extends StatelessWidget {
+  const FirstPage({super.key});
+
   static const images = [
     'images/image1.jpg',
     'images/image2.jpg',
@@ -16,7 +25,99 @@ class ExampleApp extends StatefulWidget {
     'images/image8.jpg',
     'images/image9.jpg',
   ];
-  const ExampleApp({super.key}); 
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Image.asset('images/logo/postogram.png', 
+          fit: BoxFit.contain,
+          height: 50,
+        ),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            icon: SvgPicture.asset('images/logo/3point.svg'),
+            padding: const EdgeInsets.all(8.0),
+            onPressed: () {
+                  showModalBottomSheet(
+                    context: context,
+                    builder: (context) {
+                  return Container(
+                    height: 161, // Устанавливаем фиксированную высоту
+                    child: Center(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          ListTile(
+                            leading: SvgPicture.asset(
+                              'images/logo/light_mode.svg',
+                              width: 326,
+                              height: 42,
+                            ),
+                            title: const Text('Тема'),
+                          ),
+                          ListTile(
+                            leading: SvgPicture.asset(
+                              'images/logo/upload_cloud.svg',
+                              width: 185,
+                              height: 35,
+                            ),
+                            title: const Text('Загрузить фото...'),                          
+                            ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              );
+            },
+          )
+        ],
+      ),
+      body: GridView.count(
+        crossAxisCount: 3,
+        children: List.generate(
+          images.length,
+          (index) => _GridItem(index: index, images: images),
+        ),
+      ),
+    );
+  }
+}
+
+class _GridItem extends StatelessWidget {
+  final int index;
+  final List<String> images;
+
+  const _GridItem({required this.index, required this.images, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => Navigator.push(
+        context,
+        CupertinoPageRoute(
+          builder: (_) => ExampleApp(initialPage: index, images: images),
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(2),
+        child: Image.asset(
+          images[index],
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
+
+class ExampleApp extends StatefulWidget {
+  final List<String> images;
+  final int initialPage;
+
+  const ExampleApp({super.key, required this.images, this.initialPage = 0});
 
   @override
   State<ExampleApp> createState() => _ExampleAppState();
@@ -25,94 +126,67 @@ class ExampleApp extends StatefulWidget {
 class _ExampleAppState extends State<ExampleApp> {
   static const _defaultAnimDuration = Duration(milliseconds: 300);
 
-  late var currentPage = ExampleApp.images.length ~/ 2;
+  late int currentPage = widget.initialPage;
 
   late final _pageController = PageController(
-    /// Начальным элементом будет средний элемент списка
     initialPage: currentPage,
-
-    /// Отвечает за то, сколько пространства PageView занимает активный элемент.
-    /// По умолчанию равно 1, что означает, что элемент занимает все пространство.
-    ///
-    /// Выставив такое значение, мы сможем увидеть соседние элементы.
     viewportFraction: 0.6,
   );
 
   @override
   void initState() {
-    /// С этого момента при любом изменении, произошедшем в PageController,
-    /// будет вызвать метод _onPageChanged
     _pageController.addListener(_onPageChanged);
     super.initState();
   }
 
   @override
   void dispose() {
-    /// Не забываем "отписать" нашего слушателя от контроллера и
-    /// освободить ресурсы, выделенные под него.
     _pageController
       ..removeListener(_onPageChanged)
       ..dispose();
     super.dispose();
   }
 
-  /// Здесь мы будем реагировать на изменение текущего положения в рамках PageView
   void _onPageChanged() {
-    /// Запоминаем последнюю активную страницу
     final prevPage = currentPage;
-
-    /// Вычисляем текущую (или, вернее, вычисляем страницу, к которой мы ближе всего)
     currentPage = _pageController.page?.round() ?? currentPage;
-
-    /// Если наша страница изменилась, делаем setState, чтобы вызвать перестройку виджета
     if (prevPage != currentPage) setState(() {});
   }
 
-  
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          leading: BackButton(),
-          title: Text('Date'),
-          centerTitle: true,
-          backgroundColor: Color.fromARGB(255, 255, 255, 255),
-          actions: [
-            Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Text(
-                '${currentPage + 1}/${ExampleApp.images.length}',
-                style: const TextStyle(color: Colors.black),
-              ),
+    return Scaffold(
+      appBar: AppBar(
+        leading: const BackButton(),
+        title: const Text('Date'),
+        centerTitle: true,
+        backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              '${currentPage + 1}/${widget.images.length}',
+              style: const TextStyle(color: Colors.black),
             ),
-          ],
-        ),
-        body: PageView.builder(
-          controller: _pageController,
-          itemCount: ExampleApp.images.length,
-          itemBuilder: (_, i) => Center(
-            /// Используем виджет неявной анимации, чтобы достичь
-            /// красивого эффекта плавного масштабирования
-            child: AnimatedScale(
-              duration: const Duration(milliseconds: 300),
-
-              /// Если страница является текущей, её масштаб будет оригинальным (1),
-              /// иначе - чуть меньше
-              scale: currentPage == i ? 1 : 0.7,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 10),
-                child: GestureDetector(
-                  /// Теперь при нажатии на определенный элемент мы с помощью
-                  /// PageController перемещаться к нему
-                  onTap: () => _pageController.animateToPage(
-                    i,
-                    duration: _defaultAnimDuration,
-                    curve: Curves.easeIn,
-                  ),
-                  child: _PageViewItem(image: ExampleApp.images[i]),
+          ),
+        ],
+      ),
+      body: PageView.builder(
+        controller: _pageController,
+        itemCount: widget.images.length,
+        itemBuilder: (_, i) => Center(
+          child: AnimatedScale(
+            duration: const Duration(milliseconds: 300),
+            scale: currentPage == i ? 1 : 0.7,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: GestureDetector(
+                onTap: () => _pageController.animateToPage(
+                  i,
+                  duration: _defaultAnimDuration,
+                  curve: Curves.easeIn,
                 ),
+                child: _PageViewItem(image: widget.images[i]),
               ),
             ),
           ),
@@ -123,26 +197,31 @@ class _ExampleAppState extends State<ExampleApp> {
 }
 
 class _PageViewItem extends StatelessWidget {
-  final String image; 
-  const _PageViewItem({ 
-    required this.image, 
+  final String image;
+
+  const _PageViewItem({
+    required this.image,
     super.key,
   });
-
+  
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
-      borderRadius: BorderRadius.circular(20.0),
+      borderRadius: BorderRadius.circular(16),
       child: Container(
         height: 600,
         width: 312,
         decoration: BoxDecoration(
           image: DecorationImage(
             image: AssetImage(image),
-            fit: BoxFit.cover
+            fit: BoxFit.cover,
           ),
         ),
       ),
     );
   }
+}
+
+void main() {
+  runApp(MyApp());
 }
